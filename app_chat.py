cb7dabd516c8f525d8f77412f65430125a5dca96
SUC166 <122810679+SUC166@users.noreply.github.com>
2024-01-20 15:08:04 +0100

    Update app_chat.py

--- app_chat.py
+++ app_chat.py
@@ -5,7 +5,7 @@ import streamlit as st
 import google.generativeai as genai
 from dotenv import load_dotenv
 load_dotenv()
-GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
+GOOGLE_API_KEY=os.environ.get('AIzaSyCsTBKmCMNY7Evl7aU_8QwbGFG7ltmUXnM')
 genai.configure(api_key=GOOGLE_API_KEY)
 
 new_chat_id = f'{time.time()}'
